02c668513963a21a396eca7208c6691287629c0a
3v0k4 <riccardo.odone@gmail.com>
2024-11-20 09:03:11 +0100

    feat: detect rails

--- lib/degem.rb
+++ lib/degem.rb
@@ -3,11 +3,29 @@
 require_relative "degem/version"
 
 module Degem
-  class ParseGems
+  class Gemfile
+    def initialize(definition)
+      @definition = definition
+    end
+
+    def gems
+      @gems ||= @definition.dependencies
+    end
+
+    def rails?
+      gems.map(&:name).include?("rails")
+    end
+  end
+
+  class ParseGemfile
     def call(gemfile_path)
-      Bundler::Dsl
-        .evaluate(gemfile_path, nil, {})
-        .dependencies
+      Gemfile.new(definition(gemfile_path))
+    end
+
+    private
+
+    def definition(gemfile_path)
+      Bundler::Dsl.evaluate(gemfile_path, nil, {})
     end
   end
 end
--- test/test_degem.rb
+++ test/test_degem.rb
@@ -19,10 +19,22 @@ class TestDegem < Minitest::Test
     file.close
   end
 
-  def test_it_returns_the_parsed_gems
+  def test_it_returns_the_parsed_gemfile
     with_gemfile(gems: ["foo"]) do |path|
-      actual = Degem::ParseGems.new.call(path)
-      assert_equal ["foo"], actual.map(&:name)
+      actual = Degem::ParseGemfile.new.call(path)
+      assert_equal ["foo"], actual.gems.map(&:name)
+    end
+  end
+
+  def test_it_detects_rails
+    with_gemfile(gems: ["foo"]) do |path|
+      actual = Degem::ParseGemfile.new.call(path)
+      refute actual.rails?
+    end
+
+    with_gemfile(gems: ["rails"]) do |path|
+      actual = Degem::ParseGemfile.new.call(path)
+      assert actual.rails?
     end
   end
 end
